048a56f8db8b35694564280023219413841528f2
maddie5685 <46798932+maddie5685@users.noreply.github.com>
2019-11-04 18:10:27 -0500

    Assignment 3

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
-const selected = [];
 app.use(express.static('public'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -10,8 +9,41 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }))
 let date = new Date;
 let f_date = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() 
+const mongoose = require('mongoose');
+const sendgrid = require('@sendgrid/mail');
+sendgrid.setApiKey("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");
+
+let key = "mongodb+srv://Maddie:mon_5685@cluster0-toqlt.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(key, {useNewUrlParser: true, useUnifiedTopology: true  })
+.then(()=>{
+    console.log(`Connection to database successful`);
+})
+.catch((err)=>{
+    console.log(`ERROR: ${err}`);
+})
+
+const Schema = mongoose.Schema;
+    const loginSchema = new Schema({
+        firstName : String,
+        lastName: String,
+        eMail:{
+            type: String,
+            unique: true
+        } ,
+        password: String,
+        //DOB: Date
+    })
+
+
+    const Register = mongoose.model('Register', loginSchema);
 
 app.get("/",(req,res)=>
+{
+    //res.send("Hello")
+    res.render("home")
+    
+}); 
+app.get("/login",(req,res)=>
 {
     //res.send("Hello")
     res.render("home")
@@ -53,6 +85,25 @@ app.post("/",(req,res)=>{
     
 
 })
+app.post("/login",(req,res)=>{
+    const errors =[];    
+    if (req.body.l_pword ==""){
+        errors.push("Must enter a Password!");
+    }
+    if (req.body.l_email == ""){
+        errors.push("Please enter your email!");
+    }
+
+    if(errors.length >= 1 ){
+
+        res.render("home",{
+            l_error: errors,
+        })
+
+    }    
+    
+
+})
 app.post("/registeration",(req,res)=> {
 const errors =[];
 const valid =[];
@@ -83,6 +134,11 @@ if(req.body.pword.length < 8)
 {
     errors.push("Password must be minimum 8 Characters")
 }
+let validCheck = /[!@#$^&*]/;
+if(!(req.body.pword.match(validCheck))){
+        errors.push("Password MUST contain a special character only \"!@#$%^&*\" are valid");
+    
+} 
 
 if(errors.length > 1)
 {
@@ -94,6 +150,25 @@ if(errors.length > 1)
 }  
 else
 {
+    const w_register = {
+        eMail : req.body.email,
+        firstName : req.body.fname,
+        lastName : req.body.lname,
+        password : req.body.pword
+    }
+    const s_register = new Register(w_register);
+    s_register.save()
+    .then(()=>{
+        const msg = {
+            to: w_register.eMail,
+            from: 'mallinson@myseneca.ca',
+            subject: 'Welcome To MaddieBnB',
+            text: 'Thanks for Joining!',
+            html: 'Finding a place to stay is easy with MaddieBnB',
+          }
+          sendgrid.send(msg)
+       //   res.redirect('/');
+    })
     valid.push(`Success! Welcome ${req.body.fname}`)
     res.render("register",
     {
